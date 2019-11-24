b8ae172cdcfc52f23f8bde5aa224e8929a14148a
Hugh <u88803494@gmail.com>
2019-11-24 21:32:52 +0800

    Updates

---
+++ precache-manifest.2f8cf9eca5c7737f467ec439ca9e5b72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ef77a30045542b9d7edfca3268e4bd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4cf57ec2d44dc76ef0c7",
+    "url": "./static/css/main.0ab1dfc3.chunk.css"
+  },
+  {
+    "revision": "4f735205e4989b400e62",
+    "url": "./static/js/2.7d12fc07.chunk.js"
+  },
+  {
+    "revision": "4cf57ec2d44dc76ef0c7",
+    "url": "./static/js/main.437a9dfb.chunk.js"
+  },
+  {
+    "revision": "7fe616f3a49f85e9ac66",
+    "url": "./static/js/runtime-main.1ff1632f.js"
+  }
+]);
